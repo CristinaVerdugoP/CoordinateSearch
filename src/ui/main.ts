732731393d4690c621e589712e4eb0c9ccae7901
CristinaVerdugoP <cristina.verdugo@hotmail.es>
2023-02-01 10:16:41 -0300

    fix: add ui folder in App rute

--- src/ui/main.ts
+++ src/ui/main.ts
@@ -4,17 +4,14 @@ import { Quasar } from "quasar";
 
 import "quasar/src/css/index.sass";
 
-import App from "./UI/App.vue";
-import router from "./router";
+import App from "./App.vue";
 
 import "./assets/main.css";
 import "@quasar/extras/roboto-font-latin-ext/roboto-font-latin-ext.css";
 import "@quasar/extras/material-icons/material-icons.css";
 
 const myApp = createApp(App);
-
 myApp.use(createPinia());
-myApp.use(router);
 
 myApp.use(Quasar, {
   plugins: {},
