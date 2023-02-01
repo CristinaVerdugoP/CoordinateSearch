85e44c61a50d43d0a1a9309038c204d7d487c9b6
CristinaVerdugoP <cristina.verdugo@hotmail.es>
2023-02-01 10:04:18 -0300

    feat: add a coordinates store

--- src/ui/stores/conesearch.ts
+++ src/ui/stores/conesearch.ts
@@ -1,1 +1,10 @@
-export {};
+import { defineStore } from "pinia";
+
+export const useCoordinateStore = defineStore("info", {
+  state: () => ({
+    targetName: "",
+    ra: null,
+    dec: null,
+    radius: null,
+  }),
+});
