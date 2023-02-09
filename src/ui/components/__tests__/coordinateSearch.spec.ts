276970ca4608b1c17d6225048bbf5009f2cf7c28
CristinaVerdugoP <cristina.verdugo@hotmail.es>
2023-02-09 15:32:18 -0300

    test: add new test

--- src/ui/components/__tests__/coordinateSearch.spec.ts
+++ src/ui/components/__tests__/coordinateSearch.spec.ts
@@ -18,47 +18,44 @@ describe("Store component", () => {
     const inputDec = wrapper.get('[data-test="dec"]');
     const inputRadius = wrapper.get('[data-test="radius"]');
     const inputTargetName = wrapper.get('[data-test="targetName"]');
-
     inputRa.setValue(2);
     inputDec.setValue(2);
     inputRadius.setValue(2);
     inputTargetName.setValue(2);
-
     const store = useCoordinateStore();
-
     expect(store.ra).toBe("2");
     expect(store.dec).toBe("2");
     expect(store.radius).toBe("2");
     expect(store.targetName).toBe("2");
-    store.$reset()
+    store.$reset();
   });
   it("should call resolvename on click", async () => {
-    setTestType("success")
+    setTestType("success");
     const wrapper = mount(coordinateSearch);
     const inputTargetName = wrapper.get('[data-test="targetName"]');
     inputTargetName.setValue("test target");
     const store = useCoordinateStore();
-    const button = wrapper.get('[data-test="resolve"]')
-    button.trigger("click")
-    await flushPromises()
-    expect(store.ra).toBe(1)
-    expect(store.dec).toBe(2)
-    expect(store.radius).toBe(1.5)
-    store.$reset()
+    const button = wrapper.get('[data-test="resolve"]');
+    button.trigger("click");
+    await flushPromises();
+    expect(store.ra).toBe(1);
+    expect(store.dec).toBe(2);
+    expect(store.radius).toBe(1.5);
+    store.$reset();
   });
   it("should set errror on click", async () => {
-    setTestType("error")
+    setTestType("error");
     const wrapper = mount(coordinateSearch);
     const inputTargetName = wrapper.get('[data-test="targetName"]');
     inputTargetName.setValue("test target");
     const store = useCoordinateStore();
-    const button = wrapper.get('[data-test="resolve"]')
-    button.trigger("click")
-    await flushPromises()
-    expect(store.ra).toBe(null)
-    expect(store.dec).toBe(null)
-    expect(store.radius).toBe(null)
-    expect(store.error).not.toBeNull()
-    store.$reset()
+    const button = wrapper.get('[data-test="resolve"]');
+    button.trigger("click");
+    await flushPromises();
+    expect(store.ra).toBe(null);
+    expect(store.dec).toBe(null);
+    expect(store.radius).toBe(null);
+    expect(store.error).not.toBeNull();
+    store.$reset();
   });
 });
