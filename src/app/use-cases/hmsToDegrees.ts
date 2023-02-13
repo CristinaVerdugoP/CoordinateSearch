29a2fdab6445e67ec0df1f8f3c086718576f4d2d
CristinaVerdugoP <cristina.verdugo@hotmail.es>
2023-02-12 21:41:28 -0300

    feat: add execute function

--- src/app/use-cases/hmsToDegrees.ts
+++ src/app/use-cases/hmsToDegrees.ts
@@ -1,5 +1,33 @@
-function hmsToDegrees(hrs: number, min: number, sec: number) {
-  return hrs + min / 60 + sec / 3600;
+import type { raDecHms2 } from "@/common/use-cases/typeRaDecHms";
+import type { Callbacks } from "@/common/use-cases/callbacks";
+
+function raHmsToDegrees(hrs: number, min: number, sec: number): string {
+  const degreesRa = ((hrs + min / 60 + sec / 3600) * 15).toFixed(3);
+  return degreesRa;
 }
 
-export { hmsToDegrees };
+function decHmsToDegrees(hrs: number, min: number, sec: number): string {
+  const degreesDec = (hrs + min / 60 + sec / 3600).toFixed(3);
+  return degreesDec;
+}
+
+function execute(degrees: raDecHms2, callbacks: Callbacks) {
+  type typeError = any;
+  try {
+    const resultRa = raHmsToDegrees(
+      degrees.ra.hrs,
+      degrees.ra.min,
+      degrees.ra.sec
+    );
+    const resultDec = decHmsToDegrees(
+      degrees.dec.hrs,
+      degrees.ra.min,
+      degrees.ra.sec
+    );
+    callbacks.handleSuccess(resultRa);
+    callbacks.handleSuccess(resultDec);
+  } catch (error: typeError) {
+    callbacks.handleError(error);
+  }
+}
+export { raHmsToDegrees, decHmsToDegrees, execute };
