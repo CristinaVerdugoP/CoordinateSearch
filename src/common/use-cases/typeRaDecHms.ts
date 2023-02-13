40bf771a5ffab95d3435d24f06e6c00062089b61
CristinaVerdugoP <cristina.verdugo@hotmail.es>
2023-02-12 21:48:53 -0300

    feat: add some types for ra-dec

--- src/common/use-cases/typeRaDecHms.ts
+++ src/common/use-cases/typeRaDecHms.ts
@@ -1,12 +1,35 @@
-export type hms = {
+export type raDecHms = {
+  raHms: (ra: number) => string;
+  decHms: (dec: number) => string;
+};
+
+export type hmsToDegreesResult = number;
+
+export type Ra = {
+  hrs: number;
+  min: number;
+  sec: number;
+};
+export type Dec = {
   hrs: number;
   min: number;
   sec: number;
 };
 
-export type raDecHms = {
-  raHms: number;
-  decHms: number;
+export type hmsRaDec = {
+  rahms: number;
+  dechms: number;
 };
-
-export type hmsToDegreesResult = number
+interface raDecHms2 {
+  ra: {
+    hrs: number;
+    min: number;
+    sec: number;
+  };
+  dec: {
+    hrs: number;
+    min: number;
+    sec: number;
+  };
+}
+export type { raDecHms2 };
