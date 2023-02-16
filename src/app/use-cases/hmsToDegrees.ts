2afc03c9d12a60d6ca78585d9c41f91235725dde
CristinaVerdugoP <cristina.verdugo@hotmail.es>
2023-02-16 12:15:12 -0300

    fix: changed the execute function and add a const to hms to degrees use-cases

--- src/app/use-cases/hmsToDegrees.ts
+++ src/app/use-cases/hmsToDegrees.ts
@@ -1,4 +1,7 @@
-import type { raDecHms2 } from "@/common/use-cases/typeRaDecHms";
+import type {
+  RaDecHms,
+  HmsHandleSucces,
+} from "@/common/use-cases/typeRaDecHms";
 import type { Callbacks } from "@/common/use-cases/callbacks";
 
 function raHmsToDegrees(hrs: number, min: number, sec: number): string {
@@ -10,24 +13,29 @@ function decHmsToDegrees(hrs: number, min: number, sec: number): string {
   const degreesDec = (hrs + min / 60 + sec / 3600).toFixed(3);
   return degreesDec;
 }
+export const hmsToDegreesUseCases = {
+  execute: (degrees: RaDecHms, callbacks: Callbacks) => {
+    type typeError = any; //******/
+    try {
+      const resultRa = raHmsToDegrees(
+        degrees.ra.hrs,
+        degrees.ra.min,
+        degrees.ra.sec
+      );
+      const resultDec = decHmsToDegrees(
+        degrees.dec.hrs,
+        degrees.ra.min,
+        degrees.ra.sec
+      );
+      const result: HmsHandleSucces = {
+        resultRa,
+        resultDec,
+      };
+      callbacks.handleSuccess(result);
+    } catch (error: typeError) {
+      callbacks.handleError(error);
+    }
+  },
+};
 
-function execute(degrees: raDecHms2, callbacks: Callbacks) {
-  type typeError = any;
-  try {
-    const resultRa = raHmsToDegrees(
-      degrees.ra.hrs,
-      degrees.ra.min,
-      degrees.ra.sec
-    );
-    const resultDec = decHmsToDegrees(
-      degrees.dec.hrs,
-      degrees.ra.min,
-      degrees.ra.sec
-    );
-    callbacks.handleSuccess(resultRa);
-    callbacks.handleSuccess(resultDec);
-  } catch (error: typeError) {
-    callbacks.handleError(error);
-  }
-}
-export { raHmsToDegrees, decHmsToDegrees, execute };
+export { raHmsToDegrees, decHmsToDegrees };
