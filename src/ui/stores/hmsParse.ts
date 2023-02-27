9894b3101bd4e4ddc0a0ccd0e26fcb8d70737a3d
CristinaVerdugoP <cristina.verdugo@hotmail.es>
2023-02-27 19:25:31 -0300

    fix: delete execute function

--- src/ui/stores/hmsParse.ts
+++ src/ui/stores/hmsParse.ts
@@ -1,6 +1,4 @@
-import type { Callbacks } from "@/common/use-cases/callbacks";
-
-function hmsParse(hms: string) {
+function hmsParse(hms: string): Number[] {
   const splitHms = hms.split(/:|\s+/).map((x) => +x);
   const hrs = splitHms[0];
   const min = splitHms[1];
@@ -8,17 +6,4 @@ function hmsParse(hms: string) {
   return [hrs, min, sec];
 }
 
-function hmsParseUseCases() {
-  return () => {
-    function execute(hms: string, callbacks: Callbacks) {
-      try {
-        const result = hmsParse(hms);
-        callbacks.handleSuccess(result);
-      } catch (error: any) {
-        callbacks.handleError(error);
-      }
-    }
-  }
-}
-
-export { hmsParse, hmsParseUseCases };
+export { hmsParse };
