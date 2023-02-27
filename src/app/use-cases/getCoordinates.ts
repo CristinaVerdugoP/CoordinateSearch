4674f8c291616b27836ca85767a027250e2ce362
CristinaVerdugoP <cristina.verdugo@hotmail.es>
2023-02-27 19:29:03 -0300

    fix: transformation of the function getCoordinates to async

--- src/app/use-cases/getCoordinates.ts
+++ src/app/use-cases/getCoordinates.ts
@@ -1,25 +1,28 @@
 import type { Callbacks } from "@/common/use-cases/callbacks";
 import axios from "axios";
 
-function getCoordinates() {
-  axios
-    .get(`https://cds.unistra.fr/cgi-bin/nph-sesame/-ox?${targetName}`) //********* */
+async function getCoordinates(targetName: string) {
+  const coordenadas = await axios
+    .get(`https://cds.unistra.fr/cgi-bin/nph-sesame/-ox?${targetName}`)
     .then((result) => {
       const parser = new DOMParser();
       const xmlDom = parser.parseFromString(result.data, "application/xml");
-      const jra = xmlDom.querySelector("jradeg").innerHTML;
-      const jdec = xmlDom.querySelector('jdedeg').innerHTML;
+      const jra: string = xmlDom.querySelector("jradeg")!.innerHTML;
+      const jdec: string = xmlDom.querySelector("jdedeg")!.innerHTML;
       return { jra, jdec };
     });
+  return coordenadas;
 }
 
-function execute(targetName: string, callbacks: Callbacks) {
-  try {
-    const result = getCoordinates();
-    callbacks.handleSuccess(result);
-  } catch (error) {
-    callbacks.handleError(error);
-  }
-}
-
-export { getCoordinates, execute };
+export const getCoordinatesUseCase = {
+  execute: async (targetName: string, callbacks: Callbacks) => {
+    try {
+      const result = await getCoordinates(targetName);
+      console.log("estoy en el execute", result.jra, result.jdec)
+      callbacks.handleSuccess(result);
+    } catch (error: Error) {
+      callbacks.handleError(error);
+    }
+  },
+};
+export { getCoordinates };
