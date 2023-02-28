0ef5a3290ad8cae2a9ea946086ff40438eb64f5a
CristinaVerdugoP <cristina.verdugo@hotmail.es>
2023-02-28 10:53:57 -0300

    feat: changed the hmsDegrees function and fix the resolveName function

--- src/ui/stores/conesearch.ts
+++ src/ui/stores/conesearch.ts
@@ -1,17 +1,8 @@
 import { defineStore } from "pinia";
 import { hmsToDegreesUseCases } from "@/app/use-cases/hmsToDegrees";
 import { getCoordinatesUseCase } from "@/app/use-cases/getCoordinates";
-import { hmsParse } from "@/ui/stores/hmsParse";
-import type {
-  HmsHandleSucces,
-  RaDecHms,
-} from "@/common/use-cases/typeRaDecHms";
+import type { HmsHandleSucces } from "@/common/use-cases/typeRaDecHms";
 import type { ErrorStore } from "@/common/use-cases/callbacks";
-interface Coordinates {
-  jra: string;
-  jdec: string;
-}
-
 interface TypeState {
   targetName: string;
   ra: string;
@@ -39,11 +30,6 @@ export const useCoordinateStore = defineStore("info", {
           this.ra = String(result.jra);
           this.dec = String(result.jdec);
           this.radius = 1.5;
-          console.log(
-            "Este es el action del getCoordinates",
-            this.ra,
-            this.dec
-          );
         },
         handleError: (error) => {
           this.error = error;
