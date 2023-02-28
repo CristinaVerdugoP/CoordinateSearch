44d92bad53d2c5e6bcc998defb3a0e67b7acf49d
CristinaVerdugoP <cristina.verdugo@hotmail.es>
2023-02-28 10:47:56 -0300

    fix: changed the execute function

--- src/app/use-cases/__mocks__/getCoordinates.ts
+++ src/app/use-cases/__mocks__/getCoordinates.ts
@@ -6,9 +6,10 @@ export function setTestType(ttype: string) {
   testType = ttype;
 }
 
-export function execute(targetName: string, callbacks: Callbacks) {
-  if (testType === "success")
-    callbacks.handleSuccess({ jra: 1, jdec: 2 });
-  else if (testType === "error") 
-    callbacks.handleError(new Error("error"));
-}
+export const getCoordinatesUseCase = {
+  execute: async (targetName: string, callbacks: Callbacks) => {
+    if (testType === "success")
+      callbacks.handleSuccess({ jra: "1", jdec: "2" });
+    else if (testType === "error") callbacks.handleError(new Error("error"));
+  },
+};
